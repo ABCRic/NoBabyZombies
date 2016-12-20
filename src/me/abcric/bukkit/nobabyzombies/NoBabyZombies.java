3129fa0884b18705940cefbaf57f19a7d89afa69
ABCRic <ABCRic@users.noreply.github.com>
2016-12-20 20:01:50 +0000

    Initial commit

---
+++ src/me/abcric/bukkit/nobabyzombies/NoBabyZombies.java
@@ -0,0 +1,30 @@
+package me.abcric.bukkit.nobabyzombies;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoBabyZombies extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+	getLogger().info("Starting up "
+		+ getDescription().getName()
+		+ " " + getDescription().getVersion()
+		+ " by " + String.join(", ", getDescription().getAuthors())
+		+ "...");
+	getServer().getPluginManager().registerEvents(this, this);
+	getLogger().info("Enabled.");
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntitySpawnEvent(EntitySpawnEvent event) {
+	Entity ent = event.getEntity();
+	if (ent.getType() == EntityType.ZOMBIE && ((Zombie) ent).isBaby()) {
+	    ((Zombie) ent).setBaby(false);
+	}
+    }
+}
