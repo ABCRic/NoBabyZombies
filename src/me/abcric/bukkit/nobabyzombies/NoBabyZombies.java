ea69d9568b955763d2cc18b01853954979061282
ABCRic <ABCRic@users.noreply.github.com>
2017-03-14 23:23:09 +0000

    Fix indentation

--- src/me/abcric/bukkit/nobabyzombies/NoBabyZombies.java
+++ src/me/abcric/bukkit/nobabyzombies/NoBabyZombies.java
@@ -9,22 +9,19 @@ import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoBabyZombies extends JavaPlugin implements Listener {
-    @Override
-    public void onEnable() {
-	getLogger().info("Starting up "
-		+ getDescription().getName()
-		+ " " + getDescription().getVersion()
-		+ " by " + String.join(", ", getDescription().getAuthors())
-		+ "...");
-	getServer().getPluginManager().registerEvents(this, this);
-	getLogger().info("Enabled.");
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onEntitySpawnEvent(EntitySpawnEvent event) {
-	Entity ent = event.getEntity();
-	if (ent.getType() == EntityType.ZOMBIE && ((Zombie) ent).isBaby()) {
-	    ((Zombie) ent).setBaby(false);
+	@Override
+	public void onEnable() {
+		getLogger().info("Starting up " + getDescription().getName() + " " + getDescription().getVersion() + " by "
+				+ String.join(", ", getDescription().getAuthors()) + "...");
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("Enabled.");
 	}
-    }
+
+	@EventHandler(ignoreCancelled = true)
+	public void onEntitySpawnEvent(EntitySpawnEvent event) {
+		Entity ent = event.getEntity();
+		if (ent.getType() == EntityType.ZOMBIE && ((Zombie) ent).isBaby()) {
+			((Zombie) ent).setBaby(false);
+		}
+	}
 }
